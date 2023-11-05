017b6a9bba559a10835ccaefd6be90a6e7743f19
Anette Johansen <evaajo@stud.ntnu.no>
2023-11-05 18:38:56 +0100

    Add Navbar/header

--- gruppetimer/src/App.tsx
+++ gruppetimer/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import GroupSessions from "./GroupSessions";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header> GRUPPETIMER</header>
       <GroupSessions />
     </div>
